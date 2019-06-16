7ae94a8db44a1cbba1a283b38a134dee2203f795
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2019-06-16 12:00:09 -0300

    Add design files

---
+++ CasaDoCodigo/Controllers/PedidoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CasaDoCodigo.Models;
+
+namespace CasaDoCodigo.Controllers
+{
+    public class PedidoController : Controller
+    {
+        public IActionResult Carrossel()
+        {
+            return View();
+        }
+    }
+}
